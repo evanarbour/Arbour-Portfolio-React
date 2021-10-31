a013b71b7c6ac008337dea71c5b5178f2ec64e9b
Evan Arbour <evan.arbour@gmail.com>
2021-10-31 14:17:03 -0700

    created page change functions

---
+++ react-portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Navigation from './Navigation';
+import About from './pages/About'
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+
+export default function PortfolioContainer() {
+    // setting currentPage default to the 'About' page.
+    const [currentPage, setCurrentPage] = useState('About');
+
+    // this checks to see what the currentPage is, and renders to corresponding components.
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />;
+        }
+    }
+
+    // this function takes the data (page) and passes it into the setCurrentPage to update state.
+    const handlePageChange = (page) => setCurrentPage(page)
+
+    return (
+        <div>
+            {/* we pass currentPage to set state (About) in Navigation.js and handlePageChange to update it */}
+            <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+            
+            {/* passing this to return the corresponding components */}
+            {renderPage()}
+        </div>
+    )
+
+
+
+}
