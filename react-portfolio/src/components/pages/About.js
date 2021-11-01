06fcdb76cdac0541828264ec41c8f212fb3dcfe5
Evan Arbour <evan.arbour@gmail.com>
2021-11-01 14:18:52 -0700

    styling about me page header

--- react-portfolio/src/components/pages/About.js
+++ react-portfolio/src/components/pages/About.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import '../../styles/About.css'
 
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <h1>
+        #evan-arbour &#123; <span>{<br/>}</span>
+        display: flex <span>{<br/>}</span>
+        &#125;
+      </h1>
+      <h3>
+        Performance and Strength Coach turned Developer.
+      </h3>
+        <p>
+            Hi! My name is Evan -- I grew up in
+            a small town in Maine where all my family still lives! After college, I started my fitness career
+            in NYC and then moved to Seattle in 2017.
+        </p>
+        <span>{<br/>}</span>
+        <p>
+            I am completing a Full-Stack Web Developer program through the University of Washington with skills in CSS, HTML,
+            JavaScript, and various back-end programs. I am most passionate about the UI/UX design of web applications
+            - the mixture of creative vision and technical skills.
+        </p>
+       
+      
     </div>
   );
 }
